c6ee9b458a9e4b62450b673299c8e38e40a46a48
Nicholai Nissen <nicholainissen@gmail.com>
2022-11-30 18:41:59 +0100

    inshallah 🙏

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
-  experimental: {
-    payloadExtraction: false
-  },
-  vite: {
-    assetsInclude:['**/*.m4v']
-  }
+  ssr: false
 })
