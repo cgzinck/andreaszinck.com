6633a363707a412d690879eaca896e39e164324b
Nicholai Nissen <nicholainissen@gmail.com>
2022-11-30 18:37:51 +0100

    blbalbalbalbla

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,10 @@
-export default {
-  target: 'static'
-}
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  ssr: false,
+  experimental: {
+    payloadExtraction: false
+  },
+  vite: {
+    assetsInclude:['**/*.m4v']
+  }
+})
