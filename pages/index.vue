eebd7a999fbc0e44c56df7f77ca040b43d59bea7
zinck <cgzinck@gmail.com>
2022-11-30 18:02:21 +0100

    666

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,53 @@
 <template>
-  <div>
-    Hello Nisseeee
-  </div>
-  <img src="/videos/untitled.png" alt="">
+  <!-- The video -->
+<video autoplay muted loop id="myVideo">
+  <source src="/videos/notannft.m4v" type="video/mp4">
+</video>
+
+<!-- Optional: some overlay text to describe the video -->
+<div class="content">
+  <h1>Heading</h1>
+  <p>Hello Nisse</p>
+  <!-- Use a button to pause/play the video with JavaScript -->
+  <button id="myBtn" onclick="myFunction()">Pause</button>
+</div>
 </template>
 
 <script setup></script>
 
 <style scoped>
+/* Style the video: 100% width and height to cover the entire window */
+#myVideo {
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  min-width: 100%;
+  min-height: 100%;
+}
 
+/* Add some content at the bottom of the video/page */
+.content {
+  position: fixed;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.5);
+  color: #f1f1f1;
+  width: 100%;
+  padding: 20px;
+}
+
+/* Style the button used to pause/play the video */
+#myBtn {
+  width: 200px;
+  font-size: 18px;
+  padding: 10px;
+  border: none;
+  background: #000;
+  color: #fff;
+  cursor: pointer;
+}
+
+#myBtn:hover {
+  background: #ddd;
+  color: black;
+}
 </style>
